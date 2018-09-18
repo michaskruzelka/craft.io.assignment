3b397472d08d8ed36419c90487a7dc409b5e2a89
Michael Marchanka <michaskruzelka@protonmail.ch>
2018-09-18 22:58:50 +0300

    Adding API: completed

--- api.py
+++ api.py
@@ -1,16 +1,35 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 
 
-@app.route('/jobs')
-def jobs():
-    return 'jobs'
+@app.route('/jobs/<location>')
+def jobs(location):
+    conn = pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='admin',
+        password='master123',
+        db='snap',
+        cursorclass=pymysql.cursors.DictCursor)
+    with conn.cursor() as cursor:
+        sql = "SELECT * FROM `jobs` WHERE `Location` LIKE %s"
+        cursor.execute(sql, ('%' + location + '%',))
+        rows = cursor.fetchall()
+        result = []
+        for row in rows:
+            row_string = row.get('Job Title') + ', ' + row.get('Category')\
+                         + ', ' + row.get('Status') + ', ' + row.get('Location')
+            result.append(row_string)
+        if len(result) == 0:
+            return 'No jobs found in ' + location
+        return '<br/>'.join(result)
 
 
 @app.route('/')
 def hello():
-    return 'Hello world'
+    return 'This is API'
 
 
 app.run(port=80)
